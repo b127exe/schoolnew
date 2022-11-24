310a3525e905fc417cbe9f5f32ea689264769ed8
b127exe <bilalimran6217523@gmail.com>
2022-11-24 16:02:02 +0500

    database includes

--- Documentation/db_school.sql
+++ Documentation/db_school.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2022 at 09:34 AM
+-- Generation Time: Nov 24, 2022 at 11:41 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -45,7 +45,13 @@ INSERT INTO `attendance` (`aid`, `sid`, `status`, `date`) VALUES
 (4, 3, 'absent', '2022-11-12'),
 (5, 1, 'present', '2022-11-12'),
 (6, 2, 'leave', '2022-11-11'),
-(7, 1, 'present', '2022-11-14');
+(7, 1, 'present', '2022-11-14'),
+(8, 5, 'present', '2022-11-21'),
+(9, 5, 'absent', '2022-11-19'),
+(10, 1, 'leave', '2022-11-21'),
+(11, 1, 'absent', '2022-11-19'),
+(12, 2, 'present', '2022-11-21'),
+(13, 2, 'present', '2022-11-19');
 
 -- --------------------------------------------------------
 
@@ -67,7 +73,8 @@ INSERT INTO `class` (`cid`, `cname`, `section`) VALUES
 (1, 'Class 12', 2),
 (2, 'Class 11', 2),
 (3, 'Class 10', 2),
-(4, 'Class 10', 2);
+(4, 'Class 10', 2),
+(5, 'Class 11', 1);
 
 -- --------------------------------------------------------
 
@@ -87,7 +94,8 @@ CREATE TABLE `exam` (
 --
 
 INSERT INTO `exam` (`eid`, `sid`, `exam_date`, `e_time`) VALUES
-(16, 1, '2022-12-20', '08:00');
+(16, 1, '2022-12-20', '08:00'),
+(17, 5, '2022-12-30', '13:00');
 
 -- --------------------------------------------------------
 
@@ -101,15 +109,16 @@ CREATE TABLE `exam_result` (
   `marks` int(11) NOT NULL,
   `grade` varchar(5) NOT NULL,
   `percentage` int(11) NOT NULL,
-  `exam_status` varchar(20) NOT NULL
+  `exam_status` varchar(20) NOT NULL,
+  `term` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `exam_result`
 --
 
-INSERT INTO `exam_result` (`erid`, `eid`, `marks`, `grade`, `percentage`, `exam_status`) VALUES
-(3, 16, 670, 'A', 75, 'Attend');
+INSERT INTO `exam_result` (`erid`, `eid`, `marks`, `grade`, `percentage`, `exam_status`, `term`) VALUES
+(4, 16, 890, 'A+', 80, 'Attend', '1st Semester');
 
 -- --------------------------------------------------------
 
@@ -130,7 +139,21 @@ INSERT INTO `fees` (`fid`, `amount`) VALUES
 (6, 2300),
 (7, 3400),
 (8, 5500),
-(9, 5600);
+(9, 5600),
+(10, 3400);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fees_detail`
+--
+
+CREATE TABLE `fees_detail` (
+  `fsid` int(11) NOT NULL,
+  `fid` int(11) NOT NULL,
+  `fee_month` varchar(30) NOT NULL,
+  `fee_status` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -147,6 +170,15 @@ CREATE TABLE `notice` (
   `audiance` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `notice`
+--
+
+INSERT INTO `notice` (`nid`, `nauthor`, `ntitle`, `ndesc`, `time`, `audiance`) VALUES
+(3, 'Bilal Khan', 'Result Announment Ceremony', 'Tommorrow is your result annoucment ceremony so please come along with your parent', '2022-11-18 18:38:24', 'student'),
+(4, 'Ammar Sheikh', 'Winter Vacation', 'School will remain close till 30 Nov!!', '2022-11-18 18:39:44', 'both'),
+(5, 'Bilal Khan', 'Vacation', 'School will remain open for teacher on half summer vacation.....', '2022-11-18 18:50:51', 'teacher');
+
 -- --------------------------------------------------------
 
 --
@@ -156,9 +188,9 @@ CREATE TABLE `notice` (
 CREATE TABLE `parent` (
   `pid` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `phone` int(11) NOT NULL,
+  `phone` varchar(11) NOT NULL,
   `job` varchar(50) NOT NULL,
-  `nic` int(13) NOT NULL,
+  `nic` varchar(13) NOT NULL,
   `email` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -167,10 +199,11 @@ CREATE TABLE `parent` (
 --
 
 INSERT INTO `parent` (`pid`, `name`, `phone`, `job`, `nic`, `email`) VALUES
-(2, 'Imran Khan', 2147483647, 'Office Work', 2147483647, 'imran123@gmail.com'),
-(3, 'Ikram Ahmed', 2147483647, 'Toyota Office Manager', 2147483647, 'ikran345@gmail.com'),
-(4, 'Mazhar', 2147483647, 'Sublime Architecturist', 2147483647, 'mazhar432@gmail.com'),
-(5, 'zahid', 2147483647, 'Office Work', 2147483647, 'zahid123@gmail.com');
+(2, 'Imran Khan', '03453358363', 'Office Work', '1620293252977', 'imran123@gmail.com'),
+(3, 'Ikram Ahmed', '0345667289', 'Toyota Office Manager', '1620293252966', 'ikran345@gmail.com'),
+(4, 'Mazhar', '03456672892', 'Sublime Architecturist', '1620293252954', 'mazhar432@gmail.com'),
+(5, 'zahid', '03457738292', 'Office Work', '1620293252934', 'zahid123@gmail.com'),
+(6, 'Raees ahmed', '03453358365', 'Sublime Architecturist', '1620293252933', 'raees456@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -220,7 +253,8 @@ INSERT INTO `student` (`sid`, `fname`, `lname`, `dob`, `address`, `pid`, `sgende
 (1, 'Bilal', 'Khan', '2004-11-09', 'Hno N-392 Street 04', 2, 'Male', 1, 'bilal123@gmail.com', 2, 'Student1338446'),
 (2, 'Zunaira', 'Ahmed', '2005-04-20', 'Hno T-340 Street 10', 3, 'Female', 2, 'zunaira123@gmail.com', 3, 'Student1345338'),
 (3, 'Ammar', 'Sheikh', '2004-06-22', 'Hno T-567 Street 01 korangi 3/1', 4, 'Male', 3, 'ammar543@gmail.com', 4, 'Student1345356'),
-(4, 'Ahmed', 'Khan', '2005-06-20', 'Hno Y-392 Street 04', 5, 'Male', 4, 'ahmed123@gmail.com', 5, 'Student1338466');
+(4, 'Ahmed', 'Khan', '2005-06-20', 'Hno Y-392 Street 04', 5, 'Male', 4, 'ahmed123@gmail.com', 5, 'Student1338466'),
+(5, 'Azhar', 'Rumaan', '0000-00-00', 'Hno N-392 Street 90', 6, 'Male', 5, 'azhar123@gmail.com', 6, 'Student1338466');
 
 -- --------------------------------------------------------
 
@@ -231,7 +265,7 @@ INSERT INTO `student` (`sid`, `fname`, `lname`, `dob`, `address`, `pid`, `sgende
 CREATE TABLE `subject` (
   `subid` int(11) NOT NULL,
   `subject` varchar(40) NOT NULL,
-  `detail` varchar(50) NOT NULL,
+  `detail` varchar(200) NOT NULL,
   `tid` int(11) NOT NULL,
   `fid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -241,10 +275,11 @@ CREATE TABLE `subject` (
 --
 
 INSERT INTO `subject` (`subid`, `subject`, `detail`, `tid`, `fid`) VALUES
-(2, 'Computer Science', 'Fundamental areas of computer science Computer sci', 1, 6),
+(2, 'Computer Science II', 'Computer science focuses on the development and testing of software and software systems. It involves working with mathematical models, data analysis and security, algorithms, and computational theory', 1, 6),
 (3, 'Biology', 'Fundamental areas of biology is the study of exper', 8, 7),
 (4, 'Computer Science (Python)', 'Fundamental areas of computer science Python is th', 10, 8),
-(5, 'Chemistry', 'Fundamental areas of biology is the study of exper', 10, 9);
+(5, 'Chemistry', 'Fundamental areas of biology is the study of exper', 10, 9),
+(6, 'Biology', 'Fundamental areas of biology is the study of exper', 3, 10);
 
 -- --------------------------------------------------------
 
@@ -328,6 +363,13 @@ ALTER TABLE `fees`
   ADD PRIMARY KEY (`fid`);
 
 --
+-- Indexes for table `fees_detail`
+--
+ALTER TABLE `fees_detail`
+  ADD PRIMARY KEY (`fsid`),
+  ADD KEY `fid` (`fid`);
+
+--
 -- Indexes for table `notice`
 --
 ALTER TABLE `notice`
@@ -382,43 +424,49 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `class`
 --
 ALTER TABLE `class`
-  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `exam`
 --
 ALTER TABLE `exam`
-  MODIFY `eid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `eid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `exam_result`
 --
 ALTER TABLE `exam_result`
-  MODIFY `erid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `erid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `fees`
 --
 ALTER TABLE `fees`
-  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `fees_detail`
+--
+ALTER TABLE `fees_detail`
+  MODIFY `fsid` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `notice`
 --
 ALTER TABLE `notice`
-  MODIFY `nid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `nid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `parent`
 --
 ALTER TABLE `parent`
-  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `section`
@@ -430,13 +478,13 @@ ALTER TABLE `section`
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `subid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `subid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `teacher`
@@ -448,7 +496,7 @@ ALTER TABLE `teacher`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -477,6 +525,12 @@ ALTER TABLE `exam`
 --
 ALTER TABLE `exam_result`
   ADD CONSTRAINT `exam_result_ibfk_1` FOREIGN KEY (`eid`) REFERENCES `exam` (`eid`);
+
+--
+-- Constraints for table `fees_detail`
+--
+ALTER TABLE `fees_detail`
+  ADD CONSTRAINT `fees_detail_ibfk_1` FOREIGN KEY (`fid`) REFERENCES `fees` (`fid`);
 
 --
 -- Constraints for table `student`
